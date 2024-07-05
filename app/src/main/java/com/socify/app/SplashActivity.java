acc7df7e05e0eb98eb9f87e74f0476c324f6d230
NKDuy <nkduy.dev@gmail.com>
2024-07-05 23:39:41 +0700

    chore: add user authentication

--- app/src/main/java/com/socify/app/SplashActivity.java
+++ app/src/main/java/com/socify/app/SplashActivity.java
@@ -5,16 +5,29 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
+
+  FirebaseUser firebaseUser;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+    Intent mainIntent;
+
+    firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    // redirect to home page if user is not null
+    if (firebaseUser != null) {
+      mainIntent = new Intent(this, MainActivity.class);
+    } else {
+      mainIntent = new Intent(this, LoginActivity.class);
+    }
 
     mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
     startActivity(mainIntent);
     finish();
   }
