16a70eed92cefebcbeb562a5e8e08503a2822308
NKDuy <nkduy.dev@gmail.com>
2024-07-19 18:28:30 +0700

    chore: Starting with Stories

---
+++ app/src/main/java/com/socify/app/ui/models/Story.java
@@ -0,0 +1,60 @@
+package com.socify.app.ui.models;
+
+public class Story {
+
+  private String storyId;
+  private String userId;
+  private String imageUrl;
+  private long timeStart;
+  private long timeEnd;
+
+  public Story() { }
+
+  public Story(String storyId, String userId, String imageUrl, long timeStart, long timeEnd) {
+    this.storyId = storyId;
+    this.userId = userId;
+    this.imageUrl = imageUrl;
+    this.timeStart = timeStart;
+    this.timeEnd = timeEnd;
+  }
+
+  public String getStoryId() {
+    return storyId;
+  }
+
+  public void setStoryId(String storyId) {
+    this.storyId = storyId;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public long getTimeStart() {
+    return timeStart;
+  }
+
+  public void setTimeStart(long timeStart) {
+    this.timeStart = timeStart;
+  }
+
+  public long getTimeEnd() {
+    return timeEnd;
+  }
+
+  public void setTimeEnd(long timeEnd) {
+    this.timeEnd = timeEnd;
+  }
+}
