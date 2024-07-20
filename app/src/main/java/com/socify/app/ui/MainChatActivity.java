60ae6a459dd907f0b84275cc14467c0d09ade22f
NKDuy <nkduy.dev@gmail.com>
2024-07-20 18:10:41 +0700

    chore: TabLayout and ViewPager with Fragments

--- app/src/main/java/com/socify/app/ui/MainChatActivity.java
+++ app/src/main/java/com/socify/app/ui/MainChatActivity.java
@@ -7,10 +7,16 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,7 +26,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.socify.app.R;
 import com.socify.app.SplashActivity;
+import com.socify.app.ui.fragments.ChatsFragment;
+import com.socify.app.ui.fragments.FriendsFragment;
 import com.socify.app.ui.models.User;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -60,6 +70,16 @@ public class MainChatActivity extends AppCompatActivity {
         //
       }
     });
+
+    TabLayout tabLayout = findViewById(R.id.tab_layout);
+    ViewPager viewPager = findViewById(R.id.view_pager);
+
+    ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+    viewPagerAdapter.addFragment(new ChatsFragment(), getResources().getString(R.string.chats));
+    viewPagerAdapter.addFragment(new FriendsFragment(), getResources().getString(R.string.friends));
+
+    viewPager.setAdapter(viewPagerAdapter);
+    tabLayout.setupWithViewPager(viewPager);
   }
 
   @Override
@@ -77,7 +97,41 @@ public class MainChatActivity extends AppCompatActivity {
         finish();
         return true;
     }
-    
+
     return false;
+  }
+
+  class ViewPagerAdapter extends FragmentPagerAdapter{
+
+    private ArrayList<Fragment> fragments;
+    private ArrayList<String> titles;
+
+    ViewPagerAdapter(FragmentManager fm) {
+      super(fm);
+      this.fragments = new ArrayList<>();
+      this.titles = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+      return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+      return fragments.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+      fragments.add(fragment);
+      titles.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return titles.get(position);
+    }
   }
 }
