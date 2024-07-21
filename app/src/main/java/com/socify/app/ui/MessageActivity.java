b2cc2bc9d7e8059e126274a3ceca14a07bd694cc
NKDuy <nkduy.dev@gmail.com>
2024-07-21 10:17:34 +0700

    chore: Send Message

--- app/src/main/java/com/socify/app/ui/MessageActivity.java
+++ app/src/main/java/com/socify/app/ui/MessageActivity.java
@@ -3,6 +3,8 @@ package com.socify.app.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,12 +22,17 @@ import com.google.firebase.database.ValueEventListener;
 import com.socify.app.R;
 import com.socify.app.ui.models.User;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
 
   CircleImageView profile_image;
   TextView fullname;
+
+  EditText text_send;
+  ImageButton btn_send;
 
   FirebaseUser fUser;
   DatabaseReference reference;
@@ -50,12 +57,27 @@ public class MessageActivity extends AppCompatActivity {
 
     profile_image = findViewById(R.id.profile_image);
     fullname = findViewById(R.id.fullname);
+    text_send = findViewById(R.id.text_send);
+    btn_send = findViewById(R.id.btn_send);
 
     intent = getIntent();
-    String userId = intent.getStringExtra("userId");
+    final String userId = intent.getStringExtra("userId");
 
     fUser = FirebaseAuth.getInstance().getCurrentUser();
     reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+
+    btn_send.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String msg = text_send.getText().toString();
+        if (!msg.equals("")) {
+          sendMessage(fUser.getUid(), userId, msg);
+        } else {
+          // do nothing
+        }
+        text_send.setText("");
+      }
+    });
 
     reference.addValueEventListener(new ValueEventListener() {
       @Override
@@ -70,5 +92,16 @@ public class MessageActivity extends AppCompatActivity {
         //
       }
     });
+  }
+
+  private void sendMessage(String sender, String receiver, String message) {
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+    HashMap<String, Object> hashMap = new HashMap<>();
+    hashMap.put("sender", sender);
+    hashMap.put("receiver", receiver);
+    hashMap.put("message", message);
+
+    reference.child("Chats").push().setValue(hashMap);
   }
 }
